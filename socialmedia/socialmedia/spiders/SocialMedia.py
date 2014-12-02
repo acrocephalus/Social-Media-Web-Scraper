20c546f1c0f0228ab3ac3eea148530f10a71b1c5
acrocephalus <dani.valverde@gmail.com>
2014-12-02 17:23:25 +0100

    Signed-off-by: acrocephalus <dani.valverde@gmail.com>

--- socialmedia/socialmedia/spiders/SocialMedia.py
+++ socialmedia/socialmedia/spiders/SocialMedia.py
@@ -11,10 +11,12 @@ class MySpider(Spider):
     def parse(self, response):
         items = []
         for link in response.xpath("//a"):
-            hostname = link.xpath('/html/head/link[@rel=''canonical'']').extract()
             item = SocialMediaItem()
             item['SourceTitle'] = link.xpath('/html/head/title').extract()
             item['TargetTitle'] = link.xpath('text()').extract()
             item['link'] = link.xpath('@href').extract()
-            items.append(item)
+            outbound = str(link.xpath('@href').extract())
+            if 'http' in outbound:
+                items.append(item)
         return items
+
